85e1d892108cfce32553e411f6a76dd9500c4f83
Giulia-92 <cionco.giulia@gmail.com>
2022-03-24 18:50:56 +0100

    js-biglietto-treno

---
+++ bonus/bonus.js
@@ -0,0 +1,20 @@
+let cucina = 0;
+let sala = 0;
+
+
+
+
+let sceltac = prompt('Quale cucina vuoi?');
+console.log(sceltac);
+
+
+if (sceltac==p){
+    cucina = 1; prompt('Hai scelto il modello base:Ottimo per cominciare');
+}else if (sceltac==m){
+    cucina = 2;
+    prompt('Hai scelto il nostro modello standard');
+}else{
+    cucina = 3;
+    prompt('Hai scelto il modello Delux');
+}
+
